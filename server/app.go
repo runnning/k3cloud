a7b00b17d416cc136351380321094666b1b3169b
ywn <2467634970@qq.com>
2024-01-31 15:55:33 +0800

    初始化登录

--- server/app.go
+++ server/app.go
@@ -7,11 +7,10 @@ import (
 // NewK3Cloud new application
 func NewK3Cloud(c *kernel.K3Config) (*kernel.K3Cloud, error) {
 	browser := kernel.NewBrowserWithTransport()
-	//err := browser.InitLogin(c)
-	//if err != nil {
-	//	return nil, err
-	//}
-
+	err := browser.InitLogin(c)
+	if err != nil {
+		return nil, err
+	}
 	app := &kernel.K3Cloud{
 		Config: c,
 		Client: browser,
